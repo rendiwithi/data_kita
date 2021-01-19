d6cda133e38531f6bd4472acde1186a2fff574aa
Rendi Dwi Hermawan <rendidwih2@gmail.com>
2021-01-19 20:11:07 +0700

    Layout Main Menu

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,190 +1,69 @@
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
-  double widthMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width * 1;
-  double heightMobile(BuildContext context) =>
-      MediaQuery.of(context).size.height * 1;
-
-  double getSmallDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 2 / 3;
-  double getBigDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 7 / 8;
-
-  Color mainColors = Color(0xFF5eaaa8);
-  Color secondColors = Color(0xFF00d4ff);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Stack(
         children: [
-          Positioned(
-            right: -getSmallDiameter(context) / 3,
-            top: -getSmallDiameter(context) / 3,
-            child: Container(
-              child: Center(
-                  child: Icon(
-                Icons.person_add,
-                color: Colors.white,
-              )),
-              width: getSmallDiameter(context),
-              height: getSmallDiameter(context),
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                      colors: [mainColors, secondColors],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter)),
-            ),
-          ),
-          Positioned(
-            left: -getBigDiameter(context) / 4,
-            top: -getBigDiameter(context) / 4,
-            child: Container(
-              child: Center(
-                child: Text(
-                  "Data Kita",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              width: getBigDiameter(context),
-              height: getBigDiameter(context),
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                      colors: [mainColors, secondColors],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter)),
-            ),
-          ),
           Align(
-            alignment: Alignment.bottomCenter,
-            child: ListView(
-              children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(5)),
-                  child: Column(
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(
-                            icon: Icon(
-                              Icons.email,
-                              color: mainColors,
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: mainColors)),
-                            labelText: "Email",
-                            labelStyle: TextStyle(color: mainColors)),
-                      ),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            icon: Icon(
-                              Icons.vpn_key,
-                              color: mainColors,
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: mainColors)),
-                            labelText: "Password",
-                            labelStyle: TextStyle(color: mainColors)),
-                      ),
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
-                    child: Text(
-                      "forgot Password?",
-                      style: TextStyle(color: mainColors),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 0, 20, 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        height: 40,
-                        child: Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                gradient: LinearGradient(
-                                  colors: [
-                                    mainColors,
-                                    secondColors,
-                                  ],
-                                  begin: Alignment.topCenter,
-                                  end: Alignment.bottomCenter,
-                                )),
-                            child: Material(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.transparent,
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(20),
-                                splashColor: Colors.amber,
-                                onTap: () {},
-                                child: Center(
-                                  child: Text(
-                                    "Sign In",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w700),
-                                  ),
-                                ),
-                              ),
-                            )),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () {},
-                        mini: true,
-                        elevation: 0,
-                        child: Image.network(
-                            "https://www.transparentpng.com/thumb/facebook-logo/facebook-icon-transparent-background-20.png"),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () {},
-                        mini: true,
-                        elevation: 0,
-                        child: Image.network(
-                            "https://www.transparentpng.com/thumb/facebook-logo/facebook-icon-transparent-background-20.png"),
-                      ),
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Tidak Punya Akun? ",
-                      style: TextStyle(
-                          fontSize: 11,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    Text(
-                      "Sign Up",
-                      style: TextStyle(
-                          fontSize: 11,
-                          color: mainColors,
-                          fontWeight: FontWeight.w700),
-                    ),
-                  ],
-                )
-              ],
+            alignment: FractionalOffset.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.75,
+              decoration: BoxDecoration(
+                  border: Border.all(width: 2.0, color: Colors.black),
+                  color: Color(0xff6dede5),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  )),
             ),
-          )
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.15,
+            right: 20,
+            left: 20,
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FlatButton(
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.person,
+                      size: 45,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.bookmark,
+                      size: 45,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.settings,
+                      size: 45,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+              width: MediaQuery.of(context).size.width * 0.85,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(width: 2.0, color: Colors.black),
+                color: Color(0xffa6f5ef),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+            ),
+          ),
         ],
       ),
     );
